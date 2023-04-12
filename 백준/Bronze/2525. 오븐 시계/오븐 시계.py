8c191f13d6871300212dec9f9c8e9b78df298e5b
nohheeyeon <130336617+nohheeyeon@users.noreply.github.com>
2023-04-12 13:26:58 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,14 @@
+hour, min = map(int,input().split())
+time = int(input())
+
+hour = hour + time // 60
+min = min + time % 60
+
+if min >= 60:
+    hour += 1
+    min -= 60
+
+if hour >= 24:
+    hour -= 24
+
+print(hour, min)
