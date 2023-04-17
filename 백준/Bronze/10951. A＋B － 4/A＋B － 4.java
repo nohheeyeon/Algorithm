7a2b8143e0e65c1c6bb8ba24b6de17dd373152fd
nohheeyeon <130336617+nohheeyeon@users.noreply.github.com>
2023-04-17 17:23:17 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+
+
+        while (scr.hasNext()) {
+            int a = scr.nextInt();
+            int b = scr.nextInt();
+            System.out.println(a + b);}
+    }
+}
