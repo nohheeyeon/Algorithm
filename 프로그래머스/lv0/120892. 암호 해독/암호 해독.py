638967eb78b050948343f52f3c21ad196d961762
nohheeyeon <130336617+nohheeyeon@users.noreply.github.com>
2023-04-26 01:38:28 +0900

    [level 0] Title: 암호 해독, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,5 @@
 def solution(cipher, code):
     answer = ''
-    code_str = str(code)
-    for i in range(code-1, len(cipher), int(code_str)):
+    for i in range(code-1, len(cipher), code):
         answer += cipher[i]
     return answer
