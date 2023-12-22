98de365de61fa33402c216f89656dc923b9d920b
nohheeyeon <130336617+nohheeyeon@users.noreply.github.com>
2023-12-22 23:11:24 +0900

    [level unrated] Title: 대문자와 소문자, Time: 11.21 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (char c : my_string.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                answer += (char)(c + 32);
+            } else if (c >= 'a' && c <= 'z') {
+                answer += (char)(c - 32);
+            }
+        }
+        
+        return answer;
+    }
+}
